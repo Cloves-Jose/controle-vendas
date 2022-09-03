fd95d6e699f5222682fccdee0e4ea310b62ee89c
Cloves José <clovesjsilva8@gmail.com>
2022-09-03 10:38:46 -0300

    configuração inicial

---
+++ backend/src/test/java/br/com/controleVendas/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controleVendas.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
