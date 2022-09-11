a6b56b78c672e3c5d62e1491d30ce01018c701b6
Cloves José <clovesjsilva8@gmail.com>
2022-09-11 10:02:24 -0300

    Alteracao estoque

--- backend/src/main/java/br/com/controleVendas/vendas/entities/Estoque.java
+++ backend/src/main/java/br/com/controleVendas/vendas/entities/Estoque.java
@@ -3,14 +3,18 @@ package br.com.controleVendas.vendas.entities;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import br.com.controleVendas.vendas.enums.TipoProdutoEnum;
 
+@Entity
+@Table(name = "estoque")
 public class Estoque implements Serializable{
 
 	/**
