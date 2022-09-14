686a866cfc3d4e3f1b358f64a3cd57dd30ef73b1
Cloves José <clovesjsilva8@gmail.com>
2022-09-13 21:03:41 -0300

    EstoqueRepository

--- backend/src/main/java/br/com/controleVendas/vendas/repositories/EstoqueRepository.java
+++ backend/src/main/java/br/com/controleVendas/vendas/repositories/EstoqueRepository.java
@@ -1,6 +1,8 @@
 package br.com.controleVendas.vendas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +14,15 @@ public interface EstoqueRepository extends JpaRepository<Estoque, Long>{
 	
 	@Transactional(readOnly = true)
 	Estoque findByNome(@Param("nome") String nome);
+	
+	@Modifying(clearAutomatically = true)
+	@Transactional(readOnly = false)
+	@Query("UPDATE Estoque e SET e.deletadoEm =:data WHERE e.id =:id")
+	void deleteProduto(@Param("id")Long id, @Param("data")String data);
+	
+	@Modifying(clearAutomatically = true)
+	@Transactional(readOnly = false)
+	@Query("UPDATE Estoque e SET e.deletadoEm =:valor WHERE e.id =:id")
+	void recuperarProduto(@Param("id")Long id, @Param("valor") String valor);
 	
 }
